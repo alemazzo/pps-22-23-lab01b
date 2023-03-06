68321e715008fd4f876d826ec9603cf8dc808a4b
Alessandro <alemazzoli97@gmail.com>
2023-03-06 22:45:38 +0100

    Completed refactor

--- src/e1/LogicsImpl.java
+++ src/e1/LogicsImpl.java
@@ -4,7 +4,6 @@ import e1.board.Board;
 import e1.board.BoardFactory;
 import e1.board.BoardFactoryImpl;
 import e1.movement.Position;
-import e1.piece.Piece;
 import e1.piece.PieceType;
 
 public class LogicsImpl implements Logics {
@@ -21,22 +20,13 @@ public class LogicsImpl implements Logics {
 
     @Override
     public boolean hit(final Position position) {
-
         if (!position.isValid(this.board.size())) {
             throw new IndexOutOfBoundsException();
         }
-        final Piece knight = this.board.getPiecesOfType(PieceType.KNIGHT).iterator().next();
-        final var moves = knight.getPossibleMoves(this.board.size());
-
-        if (moves.contains(position)) {
-            final boolean captured = this.board.getPieceAt(position).isPresent();
-            if (captured) {
-                this.board.removePiece(this.board.getPieceAt(position).get());
-            }
-            knight.setPosition(position);
-            return captured;
-        }
-        return false;
+        final var knight = this.board.getPiecesOfType(PieceType.KNIGHT).stream()
+                .findFirst()
+                .orElseThrow();
+        return this.board.move(knight.getPosition(), position);
     }
 
     @Override
