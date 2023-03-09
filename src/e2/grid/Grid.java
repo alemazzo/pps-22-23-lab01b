7bbe0e8752dade01f2364e4b5bd3bc4d2503f43d
Alessandro <alemazzoli97@gmail.com>
2023-03-09 16:33:31 +0100

    Refactor Grid

--- src/e2/grid/Grid.java
+++ src/e2/grid/Grid.java
@@ -6,11 +6,26 @@ import e2.cell.Cell;
 import java.util.Optional;
 import java.util.Set;
 
+/**
+ * A grid is a two-dimensional structure of cells.
+ */
 public interface Grid {
 
+    /**
+     * @return the size of this grid.
+     */
     int getSize();
 
+    /**
+     * @param position the position of the cell to get.
+     * @return the cell at the given position, if any.
+     */
     Optional<Cell> getCellAt(Position position);
 
+    /**
+     * @param position the position of the cell to get the neighbours of.
+     * @return the neighbours of the cell at the given position.
+     */
     Set<Cell> getNeighboursOfCellAt(Position position);
+    
 }
