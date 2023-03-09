4ca7cea297d05726738604b5a4b8eb04945b29eb
Alessandro <alemazzoli97@gmail.com>
2023-03-09 09:49:13 +0100

    Setup Grid and GridImpl

---
+++ src/e2/grid/GridImpl.java
@@ -0,0 +1,10 @@
+package e2.grid;
+
+public class GridImpl implements Grid {
+
+    private final int boardSize;
+
+    public GridImpl(int boardSize) {
+        this.boardSize = boardSize;
+    }
+}
--- test/e2/grid/GridTest.java
+++ test/e2/grid/GridTest.java
@@ -1,4 +1,16 @@
 package e2.grid;
 
+import org.junit.jupiter.api.BeforeEach;
+
 public class GridTest {
+
+    private final int BOARD_SIZE = 10;
+
+    private Grid grid;
+
+    @BeforeEach
+    void setup() {
+        grid = new GridImpl(BOARD_SIZE);
+    }
+    
 }
