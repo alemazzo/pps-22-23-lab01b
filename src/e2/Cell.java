f7e1e444de63cc7949c299af82de15298c9750a6
Alessandro <alemazzoli97@gmail.com>
2023-03-09 09:45:03 +0100

    Incapsuleted neighbourhood inside the Cell interface

--- src/e2/Cell.java
+++ src/e2/Cell.java
@@ -1,5 +1,9 @@
 package e2;
 
+import java.util.Set;
+
 public interface Cell {
     boolean hasMine();
+
+    Set<Cell> neighbours();
 }
--- test/e2/grid/cell/CellTest.java
+++ test/e2/grid/cell/CellTest.java
@@ -5,11 +5,13 @@ import e2.CellImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CellTest {
 
+    private static final Set<Cell> DEFAULT_NEIGHBOURS = Set.of();
     private Cell cell;
 
     @BeforeEach
@@ -20,15 +22,28 @@ public class CellTest {
     @Test
     void testCellHasNoMine() {
         final boolean hasMine = false;
-        this.cell = new CellImpl(hasMine);
+        this.cell = new CellImpl(hasMine, DEFAULT_NEIGHBOURS);
         assertFalse(cell.hasMine());
     }
 
     @Test
     void testCellHasMine() {
         final boolean hasMine = true;
-        this.cell = new CellImpl(hasMine);
+        this.cell = new CellImpl(hasMine, DEFAULT_NEIGHBOURS);
         assertTrue(cell.hasMine());
+    }
+
+
+    @Test
+    void testCellHasNeighbours() {
+        final Set<Cell> neighbours = Set.of(
+                new CellImpl(true, Set.of()),
+                new CellImpl(false, Set.of())
+        );
+        this.cell = new CellImpl(false, neighbours);
+        final var actualNeighbours = cell.neighbours();
+        assertEquals(neighbours.size(), actualNeighbours.size());
+        assertTrue(actualNeighbours.containsAll(neighbours));
     }
 
 }
